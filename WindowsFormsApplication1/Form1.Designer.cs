3cb1b0fcc2dab45a574f469e08d1d7740bc2db84
creeplays <creeplays@creeplays>
2016-02-29 01:53:19 +0500

    Signed-off-by: creeplays <creeplays@creeplays>

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(81, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(182, 100);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(0, 106);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(263, 139);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 244);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BlockAlphabetBuilder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private CheckBox[,] box=new CheckBox[5,5];
+
+        public Form1()
+        {
+             for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    box[y,x] = new CheckBox();
+                    box[y, x].Tag = y.ToString()+x.ToString();
+                    box[y, x].Text = "";
+                    box[y, x].AutoSize = true;
+                    box[y, x].Location = new Point(1+x*15, 1+y * 15);
+                    box[y, x].Click += new System.EventHandler(onChecked);
+
+                    this.Controls.Add(box[y, x]);
+                }
+            }
+            InitializeComponent();
+            codeGen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "\n"+richTextBox1.Text;
+            richTextBox1.Text = "";
+            codeGen();
+        }
+
+        private void onChecked(object sender, EventArgs e)
+        {
+            codeGen();
+        }
+
+        private void codeGen()
+        {
+            int maxInd = 0;
+            richTextBox1.Text = "alphabet.put(\'\', new int[][] {\n";
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    if (box[y, x].Checked && x > maxInd)
+                        maxInd = x;
+                }
+            }
+            for (int y = 0; y < 5; y++)
+            {
+                richTextBox1.Text += "    {";
+                for (int x = 0; x <= maxInd; x++)
+                {
+                    richTextBox1.Text += (box[y, x].Checked ? "1" : "0") + (x == maxInd ? "}" : "")+(y==4?(x == maxInd?"":",") :",")+(x == maxInd?"\n":"");
+                }
+            }
+            richTextBox1.Text += "});\n";
+        }
+    }
+}
